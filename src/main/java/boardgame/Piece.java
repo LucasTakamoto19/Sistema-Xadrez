43664800d0aaec23a1ae7e0bb8f04506c14ce221
Lucas Takamoto <lucas.takamoto@hotmail.com>
2024-02-09 15:34:28 -0300

    Criação das classes Board e Pieces

---
+++ src/main/java/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board){
+        this.board = board;
+        position = null;
+    }
+
+    // Somente classes do mesmo pacote e subclasses vão poder acessar o tabuleiro de uma peça
+    protected Board getBoard() {
+        return board;
+    }
+}
