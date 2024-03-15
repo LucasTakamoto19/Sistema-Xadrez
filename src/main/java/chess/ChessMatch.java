6d0148cca401ca1e2346d62e26ceb3a542ec1875
Lucas Takamoto <lucas.takamoto@hotmail.com>
2024-03-15 11:26:42 -0300

    Tratamento melhor para promocao

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -6,7 +6,6 @@ import boardgame.Position;
 import boardgame.exceptions.ChessException;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -96,7 +95,7 @@ public class ChessMatch {
             throw new IllegalStateException("Não há peça para ser promovida");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Tipo invalido para promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
